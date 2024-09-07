f9b5fee58e448caafcd56bd353b8380e1195c413
indrabsudirman <indrabsudirman@gmail.com>
2024-09-07 07:52:50 +0700

    Learn dictionaries

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,13 @@
+person = {
+    "name": "Indra Bayu Sudirman",
+    "live": "South Tangerang",
+    "yearDate": 1999,
+    "hobby": "Coding"
+}
+
+print(person["yearDate"])
+print(person)
+print(person.keys()) #print the key of map/dictionaries
+print(len(person))
+person["live"] = 1234
+print(person)
