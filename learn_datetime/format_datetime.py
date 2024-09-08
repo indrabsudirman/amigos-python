105545878650507118c64b20e0cf7711e03461eb
indrabsudirman <indrabsudirman@gmail.com>
2024-09-08 14:47:30 +0700

    Add learn datetime

---
+++ learn_datetime/format_datetime.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from datetime import date
+
+now  = datetime.now()
+
+print(now)
+
+print(now.strftime("%d-%m-%Y, %H:%M:%S"))
+print(now.strftime("%d-%b-%Y, %H:%M:%S"))
+print(now.strftime("%d-%B-%Y, %H:%M:%S"))
+print(date.today().strftime("%d-%B-%Y"))
+
---
+++ learn_datetime/learn_datetime.py
@@ -0,0 +1,10 @@
+# import datetime
+from datetime import datetime
+from datetime import date
+
+print(date.today())
+print(datetime.now())
+print(datetime.now().year)
+print(datetime.now().month)
+print(datetime.now().day)
+print(datetime.now().time())
